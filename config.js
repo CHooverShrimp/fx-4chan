cc8abbb45b2a6c61226424226bd59796412811db
Hoover <109660234+CHooverShrimp@users.noreply.github.com>
2025-12-02 07:05:21 -0800

    move config to config.js, add more boards

---
+++ config.js
@@ -0,0 +1,13 @@
+// config.js - Configuration and variables
+// Networking
+export const webPort = 3000;
+export const isHTTPS = true;
+
+// General Configuraiton
+export const allowNSFWBoards = true;
+export const blacklistedBoards = []; // If you don't want to proxy a board
+
+// Image Proxying - Some image sources are blocked by services such as Discord. We bypass this by proxying.
+export const allowsImageProxy = true;
+export const imageProxySrc = ["arch-img.b4k.dev"];
+export const imageProxyAge = 86400;                     // Request the services to cache for n seconds (default 24 hrs)
