26a73fb65ef05155423b7b7a635c1f6d21fb73f7
Hoover <109660234+CHooverShrimp@users.noreply.github.com>
2026-07-31 20:40:56 -0700

    disable by default

--- config.js
+++ config.js
@@ -14,6 +14,6 @@ export const imageProxySrc = ["arch-img.b4k.dev","archived.moe"];
 export const imageProxyAge = 86400;                     // Request the services to cache for n seconds (default 24 hrs)
 
 // Tor Proxying - Route outbound fetches (archive scraping, image proxying) through Tor
-export const enableTorProxy = true;
+export const enableTorProxy = false;
 export const torProxyHost = "127.0.0.1";
 export const torProxyPort = 9050;
